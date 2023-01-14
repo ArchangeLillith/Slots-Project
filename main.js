50490c51216661729f7ef2408e0b6398c80c77d2
ArchangeLillith <90073620+ArchangeLillith@users.noreply.github.com>
2023-01-14 10:11:43 -0600

    Initiated main.js, added plain text notation to track file structure

---
+++ main.js
@@ -0,0 +1,35 @@
+global variables
+
+
+Function needed: 
+
+main function 
+    get canvas here 
+
+game selection
+    able to trigger off of on load and also after button click 
+
+cash out 
+    would we handle this as a 'game over'?
+
+chunky algorithm for winning
+Well, more like a lot of these LOL 
+
+mousedown event listener 
+    this can split into handling different mouse down EventSource, like anvil and bet changes 
+
+    // function onMouseUp(evt) {
+    //     if(SELECTED_PIECE.isClose()) {
+    //         SELECTED_PIECE.snap();
+    //         if(isComplete() && END_TIME == null){
+    //             let now = new Date().getTime();
+    //             END_TIME = now;
+    //         }
+    //     }
+    //     SELECTED_PIECE = null;
+    // }
+
+This would be a good way to handle the end of the spin if modified to fit our purposes 
+    see the nested if then and how it uses different methods and gets the answers
+
+
