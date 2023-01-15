0c613c75334050e8198339178861b4edfe330e8b
ArchangeLillith <90073620+ArchangeLillith@users.noreply.github.com>
2023-01-15 14:15:13 -0600

    Moved notes to README

--- main.js
+++ main.js
@@ -14,56 +14,6 @@
 // function intitialColumnCheck()
 // function doRuinsExist()
 
-// *--------------------FILE STRUCTURE---------------------*
-* main.js 
-    Setup and requestAnimationFrame plus global variables
-* checking-algorithm.js 
-    Checking algorithm
-* scoring.js
-* ruins.js
-    state management and scoring for the ruins
-    maybe also handles the interactive parts of the different items
-* Symbol file
-    class for generic, as well as child classes with specific properties
-* Stats dump file
-    holds arrays of how much each symbol has made 
-* UI 
-    handles displaying all the animations and sound effects as well as the design of the slot machine
-* Image folder
-
-// *--------------------NEEDED ALGORITHM NOTES---------------------*
-
-// TODO: Keep in mind, what problems are we solving?
-
-function checkingForMatches(){
-    * Checking to calculate matches
-    * What symbols are matching?
-    * Are the symbols in a winning position
-    -Accounting for otters:
-        (symbol == symbol ||
-        symbol == otter)
-    * How do we decide where to check?
-    * If symbols match and are in position to score, what symbols are they (blacklist ruins in this as we have already checked that and we can store a variable from when we did saving where in the array they are and to ignore those)
-    * Turn off mousedown events while the algorithm is running so no janky stuff can happen 
-    //! Ruins check - moved into its own algorithm
-}
-
-function scoringAlgorithm(){
-    * Passed values from checking algorithm
-    * Values include
-        -Ruins items 
-        -Money 
-        -Stats 
-    * For ruins, state management for (none)/(avaliable)/(exhausted)
-        -Different visuals and sounds for each state change
-    * Money += (variable passed in from checking algorithm)
-    * Stats can be passed straight into stats.js from the main loop as things are calculated
-        -Example, [crows += variable passed in from scoring loop] before loop variable is added to the cumulative variable that will be added into global MONEY variable
-}
-
-function scoringForRuins(){
-    * 
-}
 
 // *--------------------GLOBAL VARIABLES---------------------*
 
